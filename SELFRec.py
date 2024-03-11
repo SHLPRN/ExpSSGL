718214da3ba7c7d732db2edefe49764eeafc644e
SHLPRN <20373672@buaa.edu.cn>
2024-03-11 16:48:47 +0800

    Feat: import SELFRec structure

---
+++ SELFRec.py
@@ -0,0 +1,16 @@
+from data.loader import FileIO
+
+
+class SELFRec(object):
+    def __init__(self, config):
+        self.config = config
+        self.training_data = FileIO.load_data_set(config['training.set'])
+        self.test_data = FileIO.load_data_set(config['test.set'])
+        print('Reading data and preprocessing...')
+
+    def execute(self):
+        # import the model module
+        import_str = 'from model.' + '.' + self.config['model.name'] + ' import ' + self.config['model.name']
+        exec(import_str)
+        recommender = self.config['model.name'] + '(self.config, self.training_data, self.test_data)'
+        eval(recommender).execute()
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from SELFRec import SELFRec
+from util.conf import ModelConf
+
+
 if __name__ == '__main__':
     print('ExpSSGL')
+    model = input('Please enter the model you want to run:')
+    conf = ModelConf('./conf/' + model + '.conf')
+    rec = SELFRec(conf)
